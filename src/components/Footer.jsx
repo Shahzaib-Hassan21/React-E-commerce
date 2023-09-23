1bb4398a4aef1c57301401b53f1d205f300f3f00
Shahzaib <shahzaibhassan260@gmail.com>
2023-09-23 16:22:59 +0500

    some errors fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,9 +7,9 @@ const Footer = () => {
         <div className="d-flex align-items-center justify-content-center pb-5">
           <div className="col-md-6">
             <p className="mb-3 mb-md-0">Made with ❤️ by {"Shahzaib"}
-              <a  href="https://ssahibsingh.github.io" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">Shahzaib</a>
+              <a  href="https://github.com/Shahzaib-Hassan21" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">Shahzaib</a>
             </p>
-            <a className="text-dark fs-4" href="https://github.com/ssahibsingh" target="_blank" rel="noreferrer">
+            <a className="text-dark fs-4" href="https://github.com/Shahzaib-Hassan21" target="_blank" rel="noreferrer">
               <i className="fa fa-github"></i>
             </a>
           </div>
